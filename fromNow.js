ef9db15104335f009c7399035b02d08b8a64fe4a
mojtaba kianifar <mojtaba.bank@gmail.com>
2020-04-21 16:38:49 +0430

    Create fromNow.js

---
+++ fromNow.js
@@ -0,0 +1,50 @@
+// Order does matter
+const units = [
+  ['minute', 60],
+  ['hour', 60 * 60],
+  ['day', 24 * 60 * 60],
+  ['month', 30 * 24 * 60 * 60],
+  ['year', 12 * 30 * 24 * 60 * 60],
+];
+
+export function diffUnitFor(from) {
+  const delta = Math.round((from - Date.now()) / 1000); // Rounded delta in seconds
+
+  for (let i = 0; i < units.length; i += 1) {
+    if (Math.abs(delta) < units[i][1]) {
+      const [unit, value] = units[i - 1];
+
+      // Exit the function once a unit will be found
+      return { unit, delta: Math.round(delta / value) };
+    }
+  }
+
+  return {
+    delta: Math.round(delta / (12 * 30 * 24 * 60 * 60)),
+    unit: 'year'
+  };
+}
+
+/**
+ * Calculate "time ago" or "from now" in a readable format using Intl API
+ *
+ * @param {Date|String} date
+ * @param {String} lang
+ * @returns {String}
+ */
+export default function fromNow(date, lang = 'en') {
+  let from = date;
+
+  if (!date) {
+    return '';
+  }
+
+  if (!(date instanceof Date)) {
+    from = new Date(date);
+  }
+
+  const rtf1 = new Intl.RelativeTimeFormat(lang, { style: 'long' });
+  const { delta, unit } = diffUnitFor(from);
+
+  return rtf1.format(delta, unit);
+}
